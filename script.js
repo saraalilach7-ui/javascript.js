d85d3c47381fd816c79d6b08b3d4a1df54991845
sara alilach <saraalilach7@gmail.com>
2025-09-17 16:47:59 +0100

    Modification du fichier script.js

--- script.js
+++ script.js
@@ -15,4 +15,15 @@ function addition(a, b) {
   let somme1 = addition(8, 5);
   console.log("La somme est : " + somme1);
   
+  //chapitre 9
+
+  let age = 20;
+
+if (age < 18) {
+  console.log("Vous êtes mineur.");
+} else if (age === 18) {
+  console.log("Vous venez d’atteindre la majorité !");
+} else {
+  console.log("Vous êtes majeur.");
+}
 
